9c2dfcbba00df814da0f9ebe30444680dcf766ee
Sandip6509 <sandippatel3101@gmail.com>
2024-05-01 21:52:44 +0530

    javascript

---
+++ javascript-learn/part-one/script.js
@@ -0,0 +1,52 @@
+// console.log('Hello')
+// Single line comment 
+/**
+ * multiline comment
+ */
+/**
+ * Variables
+ */
+// "use strict"
+// let myName123 = "Hello World"
+// let a = myName123;
+// console.log(a);
+
+let name = "keanu Reeves";
+name = "John Cena";
+console.log(name);
+
+/**
+ * DataTypes
+ * 1 Primitive Datatypes
+ * 1.1 Number
+ */
+let num =50;
+console.log(typeof num);
+
+//1.2 String
+let str = "Hello World"
+console.log(typeof str);
+
+// 1.3 Boolean
+let bool = false;
+console.log(typeof bool);
+
+// 1.4 null
+let a = null;
+console.log(typeof a);
+
+// 1.5 undefined
+let b;
+console.log(typeof b);
+
+/**
+ * DataTypes
+ * 2 Non Primitive Datatypes
+ * 2.1 Object
+ * 2.2 Array
+ * 2.3 Function
+ */
+
+/**
+ * 
+ */
